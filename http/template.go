cbf4cbd93bf9a661d29333e2f6303319c12faa31
MrLYC <imyikong@gmail.com>
2018-10-09 23:59:24 +0800

    add hasAttr method

--- http/template.go
+++ http/template.go
@@ -3,16 +3,13 @@ package http
 import (
 	"bytes"
 	"text/template"
+
+	"github.com/fatih/structs"
 )
 
 // Template :
 type Template struct {
 	*template.Template
-}
-
-// init :
-func (t *Template) init() {
-	t.Template.Funcs(template.FuncMap{})
 }
 
 // Render :
@@ -22,29 +19,43 @@ func (t *Template) Render(data interface{}) (string, error) {
 	return buf.String(), err
 }
 
+// FHasAttr :
+func (t *Template) FHasAttr(object interface{}, name string) bool {
+	switch value := object.(type) {
+	case map[string]interface{}:
+		_, ok := value[name]
+		return ok
+	default:
+		s := structs.New(object)
+		return t.FHasAttr(s.Map(), name)
+	}
+}
+
 // NewTemplateWithParser :
-func NewTemplateWithParser(parser func() (*template.Template, error)) (*Template, error) {
-	t, err := parser()
+func NewTemplateWithParser(name string, parser func(*template.Template) (*template.Template, error)) (*Template, error) {
+	var err error
+	tmpl := &Template{}
+	t := template.New(name).Funcs(template.FuncMap{
+		"hasAttr": tmpl.FHasAttr,
+	})
+
+	tmpl.Template, err = parser(t)
 	if err != nil {
 		return nil, err
 	}
-	tmpl := &Template{
-		Template: t,
-	}
-	tmpl.init()
 	return tmpl, nil
 }
 
 // NewTemplateFromString :
 func NewTemplateFromString(name string, content string) (*Template, error) {
-	return NewTemplateWithParser(func() (*template.Template, error) {
-		return template.New(name).Parse(content)
+	return NewTemplateWithParser(name, func(t *template.Template) (*template.Template, error) {
+		return t.Parse(content)
 	})
 }
 
 // NewTemplate :
 func NewTemplate(path string) (*Template, error) {
-	return NewTemplateWithParser(func() (*template.Template, error) {
-		return template.New(path).ParseFiles(path)
+	return NewTemplateWithParser(path, func(t *template.Template) (*template.Template, error) {
+		return t.ParseFiles(path)
 	})
 }
--- http/template_test.go
+++ http/template_test.go
@@ -16,3 +16,27 @@ func TestRender(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "ok", output)
 }
+
+func TestFHasAttr(t *testing.T) {
+	var output string
+	tmpl, err := http.NewTemplateFromString("test", `{{ if hasAttr . "OK" }}ok{{ end }}`)
+	assert.NoError(t, err)
+
+	dict := map[string]interface{}{
+		"OK": 1,
+	}
+	assert.True(t, tmpl.FHasAttr(dict, "OK"))
+	assert.False(t, tmpl.FHasAttr(dict, "_"))
+
+	output, err = tmpl.Render(dict)
+	assert.NoError(t, err)
+	assert.Equal(t, "ok", output)
+
+	object := struct{ OK int }{OK: 1}
+	assert.True(t, tmpl.FHasAttr(object, "OK"))
+	assert.False(t, tmpl.FHasAttr(object, "_"))
+
+	output, err = tmpl.Render(object)
+	assert.NoError(t, err)
+	assert.Equal(t, "ok", output)
+}
